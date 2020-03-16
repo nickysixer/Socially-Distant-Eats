1591b52ff023e67a4fad45c5a1fbc27921063cb3
Nicholas Goers <nicholas@company119.com>
2020-03-16 13:13:43 -0400

    Adding filtering and searching

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,37 @@
             i.fad.fa-spin.fa-spinner
             span Loading restaurants
       main.app__workbench(v-else)
-         //.filters
-            .filter
-               .filter__label Service
-               label
-                  input(v-model="selectedFilters", :value="null", type="radio")
-                  span All
-               label(v-for="(filter, key) in serviceFilters")
-                  input(v-model="selectedFilters", :value="key", type="radio")
-                  span {{ filter }}
+         .filters
+            .filters__filter.filters__filter--search
+               .filters__input.filters__input--text
+                  input(v-model="filters.search.input", type="search", placeholder="Search by name")
+            .filters__filter.filters__filter--city
+               .filters__input.filters__input--select
+                  select(v-model="filters.city.input", type="search", placeholder="Search by name")
+                     option(value="") Select a City
+                     option(v-for="(filter, key) in filters.city.options") {{ filter }}
+            .filters__filter.filters__filter--service
+               label.filters__input.filters__input--checkbox
+                  input(v-model="filters.service.input", value="", type="radio", checked="true")
+                  span.filters__pill
+                     i.fad.fa-check-circle
+                     span All
+               label.filters__input.filters__input--checkbox(v-for="(filter, key) in filters.service.options")
+                  input(v-model="filters.service.input", :value="key", type="radio", checked="true")
+                  span.filters__pill
+                     i.fad.fa-check-circle
+                     span {{ filter }}
+            .filters__filter.filters__filter--reset(v-if="isFiltered")
+               .filters__input.filters__input--button
+                  button.btn(@click.prevent="reset") Clear Search
+
          .directory
-            masonry.directory__list(:cols="{default: 3, 1024: 2, 680: 1}", :gutter="30")
+            masonry.directory__list(v-if="restaurants", :cols="{default: 3, 1024: 2, 680: 1}", :gutter="30")
                .directory__item(v-for="(restaurant, index) in sortedList", v-if="!!restaurant.name" :key="restaurant.name")
                   business-component(:business="restaurant")
+            .directory__empty(v-else)
+               h2 Sorry, we were unable to find any restaurants that matched your search.
+               button.btn(@click.prevent="reset") Reset Search
       footer.bottom
          span(v-html="attribution")
 </template>
@@ -45,12 +63,24 @@ export default Vue.extend({
 			restaurants: [] as Array<BusinessObject>,
 			sortBy: "name" as string,
 			filterBy: null as null | string,
-			serviceFilters: {
-				curbside_pickup: "Curbside Pickup",
-				delivery: "Delivery",
-				online_ordering: "Online Ordering"
+			filters: {
+				search: {
+					input: "" as string
+				},
+				service: {
+					input: "" as string,
+					options: {
+						curbside_pickup: "Curbside Pickup",
+						delivery: "Delivery",
+						online_ordering: "Online Ordering"
+					}
+				},
+				city: {
+					input: "" as string,
+					options: [] as Array<string>
+				}
 			},
-			selectedFilters: null as null | string,
+			selectedFilters: [] as Array<string>,
 			attribution: "SociallyDistantEats.com is the project conceived by Nick Goers of <a href='https://www.company119.com' target='_blank'>Company 119</a> to help local restaurants during the COVID-19 crisis. Please consider suggesting more local restaurants for the list!" as string
 		};
 	},
@@ -71,18 +101,82 @@ export default Vue.extend({
 			const restaurants = data.restaurants.elements;
 			this.restaurants = restaurants;
 			this.isLoading = false;
+		},
+
+		reset() {
+			this.filters.city.input = "";
+			this.filters.service.input = "";
+			this.filters.search.input = "";
 		}
 	},
 
 	computed: {
+		isFiltered(): boolean {
+			return (
+				!!this.filters.search.input ||
+				!!this.filters.city.input ||
+				!!this.filters.service.input
+			);
+		},
+
 		sortedList(): Array<BusinessObject> {
-			if (this.restaurants && this.sortBy) {
+			let list = this.filteredList;
+
+			if (this.sortBy) {
 				if (this.sortBy === "name") {
-					return _.sortBy(this.restaurants, ["name"]);
+					list = _.sortBy(list, ["name"]);
 				}
 			}
 
-			return this.restaurants;
+			return list;
+		},
+
+		filteredList(): Array<BusinessObject> {
+			let list = this.restaurants;
+
+			if (this.filters.search.input) {
+				list = list.filter((item: BusinessObject) => {
+					return item.name
+						.toLowerCase()
+						.includes(this.filters.search.input.toLowerCase());
+				});
+			}
+
+			if (this.filters.city.input) {
+				list = list.filter(
+					(item: BusinessObject) =>
+						item.city === this.filters.city.input
+				);
+			}
+
+			if (this.filters.service.input) {
+				list = list.filter(
+					(item: BusinessObject) =>
+						item[this.filters.service.input] === "TRUE"
+				);
+			}
+
+			return list;
+		},
+
+		uniqueCities(): Array<string> {
+			if (this.restaurants) {
+				const restaurants: Array<BusinessObject> = this.restaurants;
+				const uniqueCities: Array<string> = [
+					...new Set(
+						restaurants.map((item: BusinessObject) => item.city)
+					)
+				];
+				return uniqueCities.sort();
+			}
+
+			return [];
+		}
+	},
+
+	watch: {
+		uniqueCities(values) {
+			this.filters.city.options = values;
 		}
 	},
 
@@ -122,6 +216,93 @@ html,
 }
 
 .filters {
+	$filters: &;
+	display: flex;
+	margin: 0 0 1rem;
+
+	&__filter {
+		&--service {
+			display: flex;
+		}
+
+		& + & {
+			margin-left: 0.5rem;
+		}
+	}
+
+	&__input {
+		$border: 1px solid darken(white, 15);
+		$border-radius: 100px;
+		$padding: 0.5rem 1rem;
+		display: flex;
+		align-items: center;
+
+		& + & {
+			margin-left: 0.5rem;
+		}
+
+		&--select {
+			select {
+				border: $border;
+				border-radius: $border-radius;
+				appearance: none;
+				padding: $padding;
+				font-size: 0.9rem;
+				font-family: "Poppins", sans-serif;
+
+				&:focus {
+					outline: none;
+					border-color: #20c997;
+				}
+			}
+		}
+
+		&--text {
+			input {
+				padding: $padding;
+				border-radius: $border-radius;
+				border: $border;
+				font-size: 0.9rem;
+				font-family: "Poppins", sans-serif;
+
+				&:focus {
+					outline: none;
+					border-color: #20c997;
+				}
+			}
+		}
+
+		&--checkbox {
+			input {
+				opacity: 0;
+				visibility: hidden;
+				display: none;
+
+				&:checked + #{$filters}__pill {
+					border-color: #20c997;
+					background-color: #20c997;
+					color: white;
+				}
+			}
+
+			i {
+				margin-right: 0.5rem;
+			}
+
+			#{$filters}__pill {
+				border: $border;
+				border-radius: $border-radius;
+				padding: $padding;
+				display: flex;
+				align-items: center;
+			}
+
+			&:focus {
+				outline: none;
+				border-color: #20c997;
+			}
+		}
+	}
 }
 
 .directory {
@@ -145,11 +326,15 @@ html,
 		//display: inline-block;
 		margin-bottom: 1.5rem;
 	}
+
+	&__empty {
+		text-align: center;
+	}
 }
 
 .bottom {
 	text-align: center;
-	opacity: 0.75;
+	opacity: 0.5;
 	width: 800px;
 	max-width: 95%;
 	margin: 0 auto;
